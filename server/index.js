5fca6cc64f03caa682fb03bc159544d2f44bd39c
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2023-10-17 23:25:47 -0500

    Creando client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from "express";
-import logger from 'morgan';
+import logger from "morgan";
 const port = process.env.PORT ?? 3000;
 const app = express();
 
 app
-.use(logger('dev'))
-.get("/", (_, res) => {
-  res.send("<h1> Esto es el chat </h1>");
+  .use(logger("dev"))
+  .get("/", (_, res) => {
+    res.send("<h1> Esto es el chat </h1>");
   })
-.listen(port, ()=>{
-  console.log(`Servidor corriendo en el puerto ${port}`)
-});
+  .listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+  });
