73937f3887e4cdd6b5eb4d3c1b12c81d450dbdb2
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2023-10-18 23:53:26 -0500

    io.emit configurado

--- server/index.js
+++ server/index.js
@@ -1,32 +1,29 @@
 import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
-import {createServer} from "node:http";
+import { createServer } from "node:http";
 
 const port = process.env.PORT ?? 3000;
 const app = express();
 const server = createServer(app); // creando servidor http
-const io = new Server(server)
+const io = new Server(server);
 
-io.on('connection', (socket)=>{
-  console.log("Usuario conectado")
+io.on("connection", (socket) => {
+  console.log("Usuario conectado");
 
-  socket.on('disconnect', ()=>{
-    console.log("Usuario Desconectado")
-  })
-
-  socket.on("chat mensaje", (msg)=>{
-    console.log('mensaje:', msg)
-  })
-})
-
-app
-  .use(logger("dev"))
-  .get("/", (_, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
+  socket.on("disconnect", () => {
+    console.log("Usuario Desconectado");
   });
 
-
- server.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`)
+  socket.on("chat mensaje", (msg) => {
+    io.emit("chat mensaje", msg);
   });
+});
+
+app.use(logger("dev")).get("/", (_, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
