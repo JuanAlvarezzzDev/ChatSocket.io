6bac5a49e1f6e4ac3a98bf188d41c954471528bd
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2023-10-18 23:31:07 -0500

    cliente a servidor

--- server/index.js
+++ server/index.js
@@ -6,12 +6,27 @@ import {createServer} from "node:http";
 const port = process.env.PORT ?? 3000;
 const app = express();
 const server = createServer(app); // creando servidor http
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+  console.log("Usuario conectado")
+
+  socket.on('disconnect', ()=>{
+    console.log("Usuario Desconectado")
+  })
+
+  socket.on("chat mensaje", (msg)=>{
+    console.log('mensaje:', msg)
+  })
+})
 
 app
   .use(logger("dev"))
   .get("/", (_, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
-  })
-  .listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+  });
+
+
+ server.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
   });
