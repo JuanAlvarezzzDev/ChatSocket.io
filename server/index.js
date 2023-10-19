b997c197f182028c9ddc6671adcf3fe1bf6fd79f
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2023-10-18 22:58:31 -0500

    creando servidor http

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import {createServer} from "node:http";
+
 const port = process.env.PORT ?? 3000;
 const app = express();
+const server = createServer(app); // creando servidor http
 
 app
   .use(logger("dev"))
   .get("/", (_, res) => {
-    res.send("<h1> Esto es el chat </h1>");
+    res.sendFile(process.cwd() + '/client/index.html')
   })
   .listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
